073118eb52f00d37dcbacf8bab85c4dc808a0eb6
merabet-yahia <merabet.yahia.92@gmail.com>
2019-04-22 03:47:11 +0200

    Formulaire M web request contactConBld

---
+++ app/Http/Requests/ContactRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+
+
+    /**
+       *rules retourne un tableau qui contient des clés qui correspondent
+       *aux champs du formulaire et les règles de validation.
+       **personnaliser les messages d’erreurs renvoyés avec la fonction rules()**
+    **/
+
+    public function rules()
+    {
+        return [
+            'contact_name' => 'bail|required|between:1,20|alpha',
+            'contact_email' => 'bail|required|email',
+            'contact_message' => 'bail|required|max:250',
+            'comment_message' => 'bail|required|max:250'
+        ];
+    }
+}
