5bb3daaeb89e263238a742422ff68f1c0e481a68
DJEBALAMassinissa1 <djebala.massinissa@outlook.fr>
2019-04-28 02:29:22 +0200

    modification

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,6 +13,7 @@
 </div>
 
 
+<<<<<<< HEAD
 <div class="navbar-fixed-top top-bar ">
 <div class="top-bar-left px-2">
 <ul class="menu">
@@ -24,6 +25,46 @@
 </ul>
 </div>
 </div>
+=======
+ <nav class="navbar navbar-default navbar-static-top navbar-fixed-top top-bar">
+            <div class="container">
+        
+                <div class="container collapse navbar-collapse" id="app-navbar-collapse">
+               
+
+                    <ul class=" navbar-left menu">
+                                    
+                          <li>Blog</li>
+                          <li><a href="/Home">Home</a></li>
+                          <li><a href="/Articles">Article</a></li>
+                          <li><a href="/Contact">Contacte</a></li>
+                          
+                         </ul>
+
+                        <!-- Right Side Of Navbar -->
+                    <ul class=" navbar-right menu ">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                        
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+              <ul class=" navbar-right menu ">
+                <li ><a href="#"  role="button" aria-expanded="false">{{ Auth::user()->name }}   </a>
+                <li><a href="{{ route('logout') }}"onclick="event.preventDefault();document.getElementById('logout-form').submit();"> Logout</a>
+                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                  {{ csrf_field() }}
+                              </form>        
+                                  
+                          
+                          </li>
+                      </ul>
+                  </li>
+              @endif
+          </ul>
+      </div>
+            </div>
+>>>>>>> 4555213c1ecbebc0fa508eaf634ed25ff3073647
         </nav>
 </div>
 
